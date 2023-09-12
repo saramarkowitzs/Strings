8d1ce4bef7bceab81cd26d1dcf381de89f42f035
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-12 11:44:17 -0700

    uiwfea

---
+++ Strings.java
@@ -0,0 +1,10 @@
+
+public class Strings
+{
+    public static void main(String[] args) {
+        String str1="I am a string";
+        String str2=str1; 
+        String str3="I am also strig";
+        boolean stringsEqual;
+    }
+}
