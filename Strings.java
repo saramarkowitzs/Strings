6ca764116070e536fdacf551ab90e449661d1381
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-13 11:19:58 -0700

    fewoa

--- Strings.java
+++ Strings.java
@@ -4,7 +4,8 @@ public class Strings
     public static void main(String[] args) {
         String str1="I am a string";
         String str2=str1; 
-        String str3="I am also strig";
-        boolean stringsEqual;
+        String str3="I am also string";
+        boolean stringsEqual = true;
+        str1.compareTo(str3);
     }
 }
